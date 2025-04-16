978df4fffbb2828e0b8750abf5c951cc5f804fa9
juliana_assi <juliana-3@hotmail.com>
2025-04-16 09:07:43 -0300

    Deploy do Blog

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: 'https://blogpessoal-l2qs.onrender.com'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
